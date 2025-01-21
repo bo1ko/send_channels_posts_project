ac33a4deb8d1eb6394738ede94c14a2d3295111a
bo1ko <github.boykovolodymyr@gmail.com>
2025-01-21 07:12:11 +0200

    view

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'custom_admin'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_channel/', views.add_channel, name='add_channel'),
 ]
--- custom_admin/views.py
+++ custom_admin/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import TelegramChannel
 
 def index(request):
-    return render(request, 'custom_admin/index.html')
+    channels = TelegramChannel.objects.all()
+    
+    context = {
+        'channels': channels
+    }
+
+    return render(request, 'custom_admin/index.html', context)
+
+def add_channel(request):
+    return render(request, 'custom_admin/add_channel.html')
